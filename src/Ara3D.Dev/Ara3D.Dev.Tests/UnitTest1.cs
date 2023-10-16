96764c715cfa2d0396dc84843a7574505dc2cb1e
Christopher Diggins <cdiggins@gmail.com>
2023-10-16 14:45:42 -0400

    Adding source files and small change to submodule

---
+++ src/Ara3D.Dev/Ara3D.Dev.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Ara3D.Dev.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
